ad02d27148dec3a37ff707048db9afd0b2f1b5b5
Christopher Dilks <christopher.j.dilks@gmail.com>
2022-09-01 04:38:03 -0400

    feat: add PFRICH example for `Geant4OpticalTracker` SD plugin usage

---
+++ examples/OpticalTracker/scripts/run_npsim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DD4hepExamplesINSTALL=`pwd`
+source install/bin/thisOpticalTracker.sh;
+prefix=install/examples/OpticalTracker
+
+scripts/npsim.py \
+  --numberOfEvents   10                         \
+  --runType          run                        \
+  --compactFile      $prefix/compact/pfrich.xml \
+  --outputFile       sim.edm4hep.root           \
+  --enableGun        --gun.energy "30*GeV"      \
+  --gun.particle     "pi+"                      \
+  --gun.thetaMin     "195.0*deg"                \
+  --gun.thetaMax     "195.1*deg"                \
+  --gun.distribution "cos(theta)"               \
+  --part.userParticleHandler=''
